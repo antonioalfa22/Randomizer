064714553d110e0a0c9b6f3fc9e3789328b70862
antonioalfa22 <antonioalfa22@gmail.com>
2019-03-26 14:05:56 +0100

    Cambiados random

---
+++ app.js
@@ -0,0 +1,31 @@
+const axios = require("axios")
+
+const addr = "http://localhost:8081"
+const interval = 50 * 1000
+
+function randomFloat(low, high) {
+    return Math.random() * (high - low) + low
+}
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low)
+}
+
+function newDate() {
+    return new Date().getTime()
+}
+
+function sendData() {
+    const data = {
+        device: "asturias_device",
+        temperature: randomInt(-10, 50),
+        hour: newDate(),
+        gps: randomFloat(-90, 90).toString()+";"+randomFloat(-90, 90).toString()
+    }
+    axios.post(addr+"/api/dato", data).then(function(res){
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
+}
+sendData()
+setInterval(sendData, interval)
