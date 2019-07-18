bc1dfeb89760de50d29f52e02a60c1a1f657cb4b
antonioalfa22 <antonioalfa22@gmail.com>
2019-07-18 09:16:34 +0200

    Cambiados valores temperatura

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const interval = 43200000
 function randomFloat(low, high) {
     return Math.random() * (high - low) + low
 }
+
 function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low)
 }
@@ -17,11 +18,11 @@ function newDate() {
 function sendData() {
     const data = {
         device: "asturias_device",
-        temperature: randomInt(-10, 50).toString(),
+        temperature: randomInt(10, 28).toString(),
         hour: newDate().toString(),
         gps: "43.548147;-5.892345"
     }
-    axios.post(addr+"/api/dato", data).then(function(res){
+    axios.post(addr + "/api/dato", data).then(function(res) {
         console.log(res)
     }).catch(err => {
         console.log(err)
